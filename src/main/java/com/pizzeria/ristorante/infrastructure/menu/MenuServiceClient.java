04070392de7bdc04b7190c80a6d6996b7a39a7a9
joachimprinzbach <joachim.prinzbach@gmail.com>
2020-04-22 20:20:51 +0200

    Fix gitops config

--- src/main/java/com/pizzeria/ristorante/infrastructure/menu/MenuServiceClient.java
+++ src/main/java/com/pizzeria/ristorante/infrastructure/menu/MenuServiceClient.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Optional;
 import java.util.Set;
 
 @FeignClient(name = "menu-service", url = "${menu-service.url}")
